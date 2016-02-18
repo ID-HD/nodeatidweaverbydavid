1511c9c4d15c4f7c6477ac328735f3c914778702
id_hd <david@id-hd.com>
2016-02-18 12:15:58 +0000

    first test

---
+++ test/server.spec.js
@@ -0,0 +1,11 @@
+var mocha = require("mocha");
+var chai = require("chai");
+
+var expect = chai.expect;
+
+describe("When I run my first test", function(){
+   it("should expect true", function(){
+       expect(true).equal(true);
+   }); 
+});
+
