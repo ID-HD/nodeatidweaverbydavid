489541defcc9ef27ff70bd309070fe26952db3b1
id_hd <david@id-hd.com>
2016-02-25 12:22:34 +0000

    fixed #1

--- server.js
+++ server.js
@@ -6,12 +6,4 @@ app.get("/", function(req, res, next){
    res.send("Node at IDWEAVER by David!!!"); 
 });
 
-var port = process.env.PORT || 3000;
-
-app.listen(port, function(err){
-    if(err){
-        console.dir(err);
-    } else {
-        console.log("The server is listening")
-    }
-});
+module.exports = app;
--- test/server.spec.js
+++ test/server.spec.js
@@ -1,11 +1,28 @@
 var mocha = require("mocha");
 var chai = require("chai");
 
+superTest = require("supertest")
+
 var expect = chai.expect;
 
-describe("When I run my first test", function(){
-   it("should expect true", function(){
-       expect(true).equal(true);
+describe("When I start my server", function(){
+   
+   var app = require("../server");
+   var request = superTest.agent(app.listen());
+   
+   it("should return a status 200", function(done){
+       request
+         .get("/")
+         .expect(200)
+         .end(done);
    }); 
+   
+   it("should return Node at IDWEAVER by David!!!", function(done){
+       request
+         .get("/")
+         .expect("Node at IDWEAVER by David!!!")
+         .end(done);
+   }); 
+
 });
 
