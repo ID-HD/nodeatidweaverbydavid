728c1584e0c323839a1a736c42eac9db4655b0ac
id_hd <david@id-hd.com>
2016-03-03 12:14:44 +0000

    add view engine #4

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 var express = require("express");
-
+var exphbs = require("express-handlebars");
 var app = express();
 
 app.use(express.static('public'));
 
+app.engine('hbs', exphbs({
+    defaultLayout: 'main',
+    extname: '.hbs'
+}));
+
+app.set('view engine','hbs');
+
 app.get("/", function(req, res, next){
-   res.send("Node at IDWEAVER by David!!!"); 
+   res.render('home'); 
 });
 
 module.exports = app;
